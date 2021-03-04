141c40364fa87d3e81b43c17e57252f38bf95e2f
isolino <isolino@hanzo.com.br>
2021-03-04 12:03:20 -0300

    creating view packge

---
+++ app/src/main/java/br/com/isomano/mycalendar/view/MyCalendarActivity.kt
@@ -0,0 +1,14 @@
+package br.com.isomano.mycalendar.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.isomano.mycalendar.R
+
+class MyCalendarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mycalendar)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+}
---
+++ app/src/main/java/br/com/isomano/mycalendar/view/events/EventsFragment.kt
@@ -0,0 +1,6 @@
+package br.com.isomano.mycalendar.view.events
+
+import androidx.fragment.app.Fragment
+import br.com.isomano.mycalendar.R
+
+class EventsFragment : Fragment(R.layout.fragment_events)
